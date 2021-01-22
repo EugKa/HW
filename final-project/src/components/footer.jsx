cd0faada2dd56d70981c95ac8548b8e939cb94b6
eugenekovalenko <eugenekovalenko0@gmail.com>
2021-01-22 14:45:03 +0200

    add mobile and contact

--- final-project/src/components/footer.jsx
+++ final-project/src/components/footer.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import '../styles/footer.scss'
+import React from 'react';
+import '../styles/footer.scss';
+import facebook from '../assets/facebook2.png'
+import twitter from '../assets/twitter2.png'
+import instagram from '../assets/instagram2.png'
 
 export class Footer extends React.Component {
     render() {
         return (
-            <div className="footer"></div>
+            <footer className="footer">
+                <div className="footer-contacts">
+                    <div className="footer-contacts__title">CONTACT ME</div>
+                    <div className="footer-contacts__txt">
+                        <p>
+                            WRITE OR CALL ME IF YOU
+                        </p>
+                        <p>
+                            HAVE MORE QUESTIONS
+                        </p>
+                    </div>
+                    <div className="footer-contacts__txt">
+                        <p>
+                            INFO@MYSITE.COM
+                        </p>
+                        <p>
+                            123-456-7890
+                        </p>
+                    </div>
+                    <div className="footer-contacts__links">
+                        <a href="https://www.facebook.com/wix">
+                            <img className="footer-contacts__img" src={facebook} alt="" />
+                        </a>
+                        <a href="https://twitter.com/wix">
+                            <img className="footer-contacts__img" src={twitter} alt="" />
+                        </a>
+                        <a href="https://www.instagram.com/wix/">
+                            <img className="footer-contacts__img" src={instagram} alt="" />
+                        </a>
+                    </div>
+                </div>
+                <div className="footer-form">
+                    <form className="form">
+                        <input className="form__input form__input_name" type="text" placeholder="Name" />
+                        <input className="form__input form__input_email" type="text" placeholder="Email" />
+                        <input className="form__input form__input_subject" type="text" placeholder="Subject" />
+                        <textarea className="form__input  form__input_massage" type="text" placeholder="Massage" />
+                        <button className="form__btn" type="submit">Submit</button>
+                    </form>
+                    <div className="footer-form__aside">
+                        <a href="http://">Up</a>
+                        <div className="footer-form__created">
+                            ©2023 BY JOEY DIXON. PROUDLY CREATED WITH
+                        </div>
+                    </div>
+                </div>
+            </footer>
         )
     }
 }
--- final-project/src/components/mobile.jsx
+++ final-project/src/components/mobile.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import '../styles/mobile.scss'
+import React from 'react';
+import '../styles/mobile.scss';
+import scree from '../assets/scree.png';
+import google from '../assets/google.png';
+import apple from '../assets/apple.png';
 
 export class Mobile extends React.Component {
     render() {
         return (
-            <div className="mobile"></div>
+            <div className="mobile">
+                <div className="mobile__wrapper">
+                    <div className="mobile-screen">
+                        <img className="mobile__img" src={scree} alt="#" />
+                    </div>
+                    <div className="mobile-join">
+                        <div className="mobile-join__caption">
+                            <h1 className="mobile-join__title">Join us on mobile</h1>
+                            <div className="mobile-join__subt">
+                                The Wix App lets you stay updated on the go. Just add your phone number and we’ll text you a link.
+                            </div>
+                        </div>
+                        <form className="mobile-join__form">
+                            <input className="mobile-join__input mobile-join__input-country" placeholder="+380" type="tel" />
+                            <input className="mobile-join__input mobile-join__input-phone" placeholder="Phone Number" type="tel" />
+                            <button type="submit" className="mobile-join__btn">Send</button>
+                        </form>
+                        <div className="mobile-join__links">
+                            <a href="https://apps.wix.com/place-invites/invite-lp/ffb304c2-ebb7-4728-83c1-9912da052236?ref=symp_button">
+                                <img className="mobile-join__img" src={google} alt="" />
+                            </a>
+                            <a href="https://apps.wix.com/place-invites/invite-lp/ffb304c2-ebb7-4728-83c1-9912da052236?ref=symp_button">
+                                <img className="mobile-join__img" src={apple} alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
